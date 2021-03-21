000da2d62e7ffdde087d71a5ec054114c8ef5072
Dedigraz <ebunrereadebanjo@outlook.com>
2021-03-21 08:25:41 +0100

    B3
    Got Animation working

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 	public Rigidbody2D rb;
 	public float movespeed = 5f;
 	private Vector2 movement;
+	public Animator animator;
 
 	// Update is called once per frame
 	void Update()
@@ -14,6 +15,10 @@ public class PlayerMovement : MonoBehaviour
 		//Input
 		movement.x =Input.GetAxisRaw("Horizontal");
 		movement.y = Input.GetAxisRaw("Vertical");
+
+		animator.SetFloat("horizontal", movement.x);
+		animator.SetFloat("vertical", movement.y);
+		animator.SetFloat("speed", movement.sqrMagnitude);
 	}
 
 	private void FixedUpdate()
