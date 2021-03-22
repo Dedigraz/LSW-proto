22d96afdaf25a0b678fab685e56f30a78fc93946
Dedigraz <ebunrereadebanjo@outlook.com>
2021-03-22 13:37:06 +0100

    B6

---
+++ Assets/Resources/Scripts/AttireRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class AttireRenderer : MonoBehaviour
+{
+	public static SpriteRenderer rend;
+	public Sprite DefaultSprite, BeachSprite;
+	public Animator animator;
+	public  RuntimeAnimatorController defaultAnimatorController = Resources.Load<RuntimeAnimatorController>("Animations/Player");
+	public  RuntimeAnimatorController beachAnimatorController = Resources.Load<RuntimeAnimatorController>("Animations/beachAnimation/Beach Costume");
+
+	private void Awake()
+	{
+		rend = GetComponent<SpriteRenderer>();
+		rend.sprite = DefaultSprite;
+		animator.GetComponent<Animator>();
+		animator.runtimeAnimatorController = defaultAnimatorController;
+	}
+	public void ChangeSprite()
+	{
+		if (rend.sprite == DefaultSprite)
+		{
+			rend.sprite = BeachSprite;
+			animator.runtimeAnimatorController = beachAnimatorController;
+		}
+		if(rend.sprite == BeachSprite)
+		{
+			rend.sprite = DefaultSprite;
+			animator.runtimeAnimatorController = defaultAnimatorController;
+		}
+	}
+
+}
---
+++ Assets/Resources/Scripts/InventorySlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+	public Image icon;
+	public GameObject InventoryObj;
+
+	private void Update()
+	{
+		
+	}
+	public void UseItem()
+	{ 
+		FindObjectOfType<AttireRenderer>().ChangeSprite();
+		FindObjectOfType<InventoryUI>().EndWindow();
+	}
+
+	//public override void Use()
+	//{
+	//	base.Use();
+
+	//	ChangeSprite(item);
+	//}
+
+}
